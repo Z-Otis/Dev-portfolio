f30f86d06c98be693533f9201b0e781bfed984f7
Zubby <ibeh.enoch2000@gmail.com>
2024-12-28 11:18:48 +0100

    first commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,103 @@
+import { PerspectiveCamera } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import React, { Suspense } from 'react'
+import HackerRoom from '../components/HackerRoom'
+import CanvasLoader from '../components/CanvasLoader'
+import { Leva, useControls } from 'leva'
+import { useMediaQuery } from 'react-responsive'
+import { calculateSizes } from '../constants.js/index.js'
+import Target from '../components/Target.jsx'
+import ReactLogo from '../components/ReactLogo.jsx'
+import Cube from '../components/Cube.jsx'
+import Rings from '../components/Rings.jsx'
+import HeroCamera from '../components/HeroCamera.jsx'
+import Button from '../components/Button.jsx'
+
+const Hero = () => {
+
+    // const x = useControls('HackerRoom', {
+    //     positionX: {
+    //         value: 2.5,
+    //         min: -10,
+    //         max: 10    
+    //     },
+    //     positionY: {
+    //         value: -15.7,
+    //         min: -20,
+    //         max: 10    
+    //     },
+    //     positionZ: {
+    //         value: 2.5,
+    //         min: -10,
+    //         max: 10    
+    //     },
+    //     rotationX: {
+    //         value: 0,
+    //         min: -10,
+    //         max: 10    
+    //     },
+    //     rotationY: {
+    //         value: -3.2,
+    //         min: -10,
+    //         max: 10    
+    //     },
+    //     rotationZ: {
+    //         value: 0,
+    //         min: -10,
+    //         max: 10    
+    //     },
+    //     scale: {
+    //         value: 0.15,
+    //         min: 0.1,
+    //         max: 1    
+    //     }
+    // });
+    const isMobile = useMediaQuery({ maxWidth: 768 });
+    const isSmall = useMediaQuery({ maxWidth: 480 });
+    const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+    const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+  return (
+    <section className='min-h-screen w-full flex flex-col relative border-2 border-blue-500'>
+      <div className="w-full mx-auto flex flex-col sm:mt-30 mt-20 c-space gap-3">
+        <p className='sm:text-3xl text-2xl font-medium text-white text-center font-generalsans'>Hi, I am Enoch <span className="waving-hand">👋</span></p>
+        <p className='hero_tag text-gray_gradient'>Building Products and Brands</p>
+      </div>
+      <div className="w-full h-full absolute inset-0">
+      {/* <Leva /> */}
+        <Canvas className='w-full h-full'>
+            <Suspense fallback={<CanvasLoader />}>
+            
+            <PerspectiveCamera makeDefault position={[0, 0, 20]} />
+            <HeroCamera isMobile={isMobile} >
+                <HackerRoom 
+                position={sizes.deskPosition} 
+                rotation={[0, -Math.PI, 0]}
+                scale={sizes.deskScale} 
+                />
+            </HeroCamera>
+           
+            <group>
+              <Target position={sizes.targetPosition} />
+              <ReactLogo position={sizes.reactLogoPosition} />
+              <Cube position={sizes.cubePosition} />
+              <Rings position={sizes.ringPosition}/>
+            </group>
+
+            <ambientLight intensity={1} />
+            <directionalLight intensity={0.5} position={[10, 10, 10]} />
+            </Suspense>
+        </Canvas>
+      </div>
+      <div className='absolute bottom-7  left-0 right-0 w-full  z-10 c-space'>
+        <a href="#about" className='w-fit'>
+          <Button name="Let's work together" isBeam containerClass="sm:w-fit w-full sm:min-w-96" />
+       
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
